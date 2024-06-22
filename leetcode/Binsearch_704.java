71f0924726ef90b7d0e334e8bed764a9575ca433
RosyRivers <1599871125@qq.com>
2024-06-22 11:37:59 +0800

    第一次完成704和27

---
+++ leetcode/Binsearch_704.java
@@ -0,0 +1,55 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Binsearch_704 {
+    public static void main(String[] args) {
+        try (var in = new Scanner(System.in)) {
+            String cache = in.nextLine();
+            int target = in.nextInt();
+            ArrayList<Integer> numbers = new ArrayList<>();
+            String[] num = cache.split(" ");
+            for (String string : num) {
+                int temp = Integer.parseInt(string);
+                numbers.add(temp);
+            }
+            int []nums = new int[numbers.size()];
+            int i = 0;
+            for (Integer number : numbers) {
+                nums[i] = number;
+                i++;            
+            }
+            System.out.println(Solution704.search(nums, target));
+        } catch (NumberFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+}
+
+class Solution704 {
+    public static int search(int[] nums, int target) {
+        int end = nums.length - 1;
+        int start = 0;
+        int middle = (start + end) / 2;
+        while (nums[middle] != target) {
+            if(target > nums[middle]) { 
+                start = middle;
+                middle = (start + end) / 2;
+            }
+            else {
+                end = middle;
+                middle = (start + end) / 2;
+            }
+            if(start >= end - 1) {
+                break;
+            }
+        }
+        if(nums[middle] == target) return middle;
+        if(nums[end] == target) return end;
+        return -1;
+
+    }
+}
