a059d592f326effd0cdd91f8e6550c021feeb19c
RosyRivers <1599871125@qq.com>
2024-06-27 16:40:25 +0800

    迭代-翻转链表

---
+++ leetcode/206.reverse-linked-list.java
@@ -0,0 +1,108 @@
+
+
+/*
+ * @lc app=leetcode.cn id=206 lang=java
+ * @lcpr version=30204
+ *
+ * [206] 反转链表
+ *
+ * https://leetcode.cn/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (74.49%)
+ * Likes:    3604
+ * Dislikes: 0
+ * Total Accepted:    1.9M
+ * Total Submissions: 2.6M
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：head = [1,2]
+ * 输出：[2,1]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入：head = []
+ * 输出：[]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 链表中节点的数目范围是 [0, 5000]
+ * -5000 <= Node.val <= 5000
+ * 
+ * 
+ * 
+ * 
+ * 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+ * 
+ * 
+ * 
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+
+ // Definition for singly-linked list.
+// class ListNode {
+//       int val;
+//       ListNode next;
+//       ListNode() {}
+//       ListNode(int val) { this.val = val; }
+//       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//  }
+ 
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        // 两个指针，一个指向当前节点，一个指向前一个节点
+        var index = head;
+        ListNode prev = null;
+        // 迭代
+        while(index != null) {
+            var temp = index.next;
+            index.next = prev;
+            // 两个索引进行移动
+            prev = index;
+            index = temp;
+        }
+        return prev;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+ */
+
