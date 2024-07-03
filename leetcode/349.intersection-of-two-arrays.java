339a45b6daabbda895247a1dd5da50efd943b619
RosyRivers <1599871125@qq.com>
2024-07-03 17:18:28 +0800

    hash标准解答

---
+++ leetcode/349.intersection-of-two-arrays.java
@@ -0,0 +1,101 @@
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * @lc app=leetcode.cn id=349 lang=java
+ * @lcpr version=30204
+ *
+ * [349] 两个数组的交集
+ *
+ * https://leetcode.cn/problems/intersection-of-two-arrays/description/
+ *
+ * algorithms
+ * Easy (74.66%)
+ * Likes:    914
+ * Dislikes: 0
+ * Total Accepted:    581.5K
+ * Total Submissions: 778.5K
+ * Testcase Example:  '[1,2,2,1]\n[2,2]'
+ *
+ * 给定两个数组 nums1 和 nums2 ，返回 它们的 交集 。输出结果中的每个元素一定是 唯一 的。我们可以 不考虑输出结果的顺序 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出：[2]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出：[9,4]
+ * 解释：[4,9] 也是可通过的
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums1.length, nums2.length <= 1000
+ * 0 <= nums1[i], nums2[i] <= 1000
+ * 
+ * 
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+            return new int[0];
+        }
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> resSet = new HashSet<>();
+        //遍历数组1
+        for (int i : nums1) {
+            set1.add(i);
+        }
+        //遍历数组2的过程中判断哈希表中是否存在该元素
+        for (int i : nums2) {
+            if (set1.contains(i)) {
+                resSet.add(i);
+            }
+        }
+      
+        //方法1：将结果集合转为数组
+
+        return resSet.stream().mapToInt(x -> x).toArray();
+        
+        // //方法2：另外申请一个数组存放setRes中的元素,最后返回数组
+        // int[] arr = new int[resSet.size()];
+        // int j = 0;
+        // for(int i : resSet){
+        //     arr[j++] = i;
+        // }
+        
+        // return arr;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,2,1]\n[2,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [4,9,5]\n[9,4,9,8,4]\n
+// @lcpr case=end
+
+ */
+
