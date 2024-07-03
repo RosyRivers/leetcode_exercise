8eb3f815ddb38505e01b61d7ad859c4158572876
RosyRivers <1599871125@qq.com>
2024-07-03 22:25:05 +0800

    HashMap完成赎金券

---
+++ leetcode/383.ransom-note.java
@@ -0,0 +1,101 @@
+
+import java.util.HashMap;
+
+/*
+ * @lc app=leetcode.cn id=383 lang=java
+ * @lcpr version=30204
+ *
+ * [383] 赎金信
+ *
+ * https://leetcode.cn/problems/ransom-note/description/
+ *
+ * algorithms
+ * Easy (64.59%)
+ * Likes:    892
+ * Dislikes: 0
+ * Total Accepted:    505.7K
+ * Total Submissions: 781.8K
+ * Testcase Example:  '"a"\n"b"'
+ *
+ * 给你两个字符串：ransomNote 和 magazine ，判断 ransomNote 能不能由 magazine 里面的字符构成。
+ * 
+ * 如果可以，返回 true ；否则返回 false 。
+ * 
+ * magazine 中的每个字符只能在 ransomNote 中使用一次。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：ransomNote = "a", magazine = "b"
+ * 输出：false
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：ransomNote = "aa", magazine = "ab"
+ * 输出：false
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入：ransomNote = "aa", magazine = "aab"
+ * 输出：true
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= ransomNote.length, magazine.length <= 10^5
+ * ransomNote 和 magazine 由小写英文字母组成
+ * 
+ * 
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> hash = new HashMap<>();
+        for (int i = 0; i < magazine.length(); i++) {
+            hash.put(magazine.charAt(i), 
+                hash.getOrDefault(magazine.charAt(i), 0) + 1);
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char ch = ransomNote.charAt(i);
+            if (hash.containsKey(ch) && hash.get(ch) > 1) {
+                hash.put(ch, hash.get(ch) - 1);
+            }
+            else if (hash.containsKey(ch) && hash.get(ch) == 1) {
+                hash.remove(ch);
+            }
+            else return false;
+        }
+        return true;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "a"\n"b"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "aa"\n"ab"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "aa"\n"aab"\n
+// @lcpr case=end
+
+ */
+
