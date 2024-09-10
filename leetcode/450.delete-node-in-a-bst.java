fbb7cfdc0c58eecf71ef0a67a9f7d6a684804ef1
RosyRivers <1599871125@qq.com>
2024-09-10 15:42:09 +0800

    二叉树第七天

---
+++ leetcode/450.delete-node-in-a-bst.java
@@ -0,0 +1,137 @@
+/*
+ * @lc app=leetcode.cn id=450 lang=java
+ * @lcpr version=30204
+ *
+ * [450] 删除二叉搜索树中的节点
+ *
+ * https://leetcode.cn/problems/delete-node-in-a-bst/description/
+ *
+ * algorithms
+ * Medium (52.34%)
+ * Likes:    1368
+ * Dislikes: 0
+ * Total Accepted:    281.6K
+ * Total Submissions: 536.5K
+ * Testcase Example:  '[5,3,6,2,4,null,7]\n3'
+ *
+ * 给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的 key
+ * 对应的节点，并保证二叉搜索树的性质不变。返回二叉搜索树（有可能被更新）的根节点的引用。
+ * 
+ * 一般来说，删除节点可分为两个步骤：
+ * 
+ * 
+ * 首先找到需要删除的节点；
+ * 如果找到了，删除它。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 
+ * 输入：root = [5,3,6,2,4,null,7], key = 3
+ * 输出：[5,4,6,2,null,null,7]
+ * 解释：给定需要删除的节点值是 3，所以我们首先找到 3 这个节点，然后删除它。
+ * 一个正确的答案是 [5,4,6,2,null,null,7], 如下图所示。
+ * 另一个正确答案是 [5,2,6,null,4,null,7]。
+ * 
+ * 
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: root = [5,3,6,2,4,null,7], key = 0
+ * 输出: [5,3,6,2,4,null,7]
+ * 解释: 二叉树不包含值为 0 的节点
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: root = [], key = 0
+ * 输出: []
+ * 
+ * 
+ * 
+ * 提示:
+ * 
+ * 
+ * 节点数的范围 [0, 10^4].
+ * -10^5 <= Node.val <= 10^5
+ * 节点值唯一
+ * root 是合法的二叉搜索树
+ * -10^5 <= key <= 10^5
+ * 
+ * 
+ * 
+ * 
+ * 进阶： 要求算法时间复杂度为 O(h)，h 为树的高度。
+ * 
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        return binTravel(root, key);
+
+    }
+    public TreeNode binTravel(TreeNode node, int key) {
+        if (node == null) return null;
+        if (key > node.val) node.right = binTravel(node.right, key);
+        else if (key < node.val) node.left = binTravel(node.left, key);
+        else {
+            if (node.left != null && node.right != null) {
+                TreeNode temp = node.right;
+                while (temp.left != null) {
+                    temp = temp.left;
+                }
+                temp.left = node.left;
+                return node.right;
+            }
+            else if (node.left == null && node.right != null)
+                return node.right;
+            else if (node.left != null && node.right == null)
+                return node.left;
+            else return null;
+        }
+        return node;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [5,3,6,2,4,null,7]\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [5,3,6,2,4,null,7]\n0\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n0\n
+// @lcpr case=end
+
+ */
+
