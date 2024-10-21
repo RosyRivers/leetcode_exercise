d323d628be40cd3201c6e10cf7b3d8c2223b4ccb
RosyRivers <1599871125@qq.com>
2024-10-21 14:19:06 +0800

    图论完结

--- leetcode/DAGtong.java
+++ leetcode/DAGtong.java
@@ -4,29 +4,30 @@ import java.util.Scanner;
 
 public class DAGtong {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int m = in.nextInt();
-        int [][] matrix = new int[n][n];
-        for (int i = 0; i < m; i++) {
-            matrix[in.nextInt() - 1][in.nextInt() - 1] = 1;
-        }
-        ArrayDeque<Integer> queue = new ArrayDeque<>();
-        HashSet<Integer> hasVisited = new HashSet<>();
-        queue.offer(0);
-        hasVisited.add(0);
-        while (!queue.isEmpty()) {
-            int curNode = queue.poll();
-            for (int i = 0; i < n; i++) {
-                if (hasVisited.contains(i)) continue;
-                if (matrix[curNode][i] == 1) {
-                    queue.offer(i);
-                    hasVisited.add(i);
+        try (Scanner in = new Scanner(System.in)) {
+            int n = in.nextInt();
+            int m = in.nextInt();
+            int [][] matrix = new int[n][n];
+            for (int i = 0; i < m; i++) {
+                matrix[in.nextInt() - 1][in.nextInt() - 1] = 1;
+            }
+            ArrayDeque<Integer> queue = new ArrayDeque<>();
+            HashSet<Integer> hasVisited = new HashSet<>();
+            queue.offer(0);
+            hasVisited.add(0);
+            while (!queue.isEmpty()) {
+                int curNode = queue.poll();
+                for (int i = 0; i < n; i++) {
+                    if (hasVisited.contains(i)) continue;
+                    if (matrix[curNode][i] == 1) {
+                        queue.offer(i);
+                        hasVisited.add(i);
+                    }
                 }
             }
+            if (hasVisited.size() == n) 
+                System.out.println(1);
+            else System.out.println(-1);
         }
-        if (hasVisited.size() == n) 
-            System.out.println(1);
-        else System.out.println(-1);
     }
 }
